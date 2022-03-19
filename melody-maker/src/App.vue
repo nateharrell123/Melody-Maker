07d657f4767a4868156fa60698fca5243cdf16f8
Nate Harrell <nate.harrell@me.com>
2022-03-19 15:26:05 -0500

    create

---
+++ melody-maker/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div >
+    <Title />
+  </div>
+</template>
+
+<script>
+import Title from "@/components/Title.vue"
+export default {
+  components: {
+    Title
+  }
+}
+</script>
+<style lang="scss">
+
+body {
+  background-color: #32363B;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ melody-maker/src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hey there ajskdlf
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Title"
+}
+</script>
+
+<style scoped>
+
+</style>
