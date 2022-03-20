48c6334c4bbbf93d5c719b7ca6167e3e2ead707d
Nate Harrell <nate.harrell@me.com>
2022-03-20 18:21:12 -0500

    animation

--- melody-maker/src/components/Title.vue
+++ melody-maker/src/components/Title.vue
@@ -1,12 +1,21 @@
 <template>
-    <div class="title"> 
+<header>
+    <div class="title">
         Let's get started.
     </div>
+</header>
 </template>
 
 <script>
 export default {
-    name: "Title"
+    name: "Title",
+    data(){
+        return{
+        }
+    },
+    mounted:{
+
+    }
 }
 </script>
 
@@ -19,5 +28,21 @@ export default {
   text-align: center;
   font-size:75px;
   text-transform: uppercase;
+
+  transition: transform 1s;
 }
+@keyframes slideInTop {
+    0% {
+        transform: translateY(700%);
+        opacity: ;
+    }
+    100% {
+        transform: translateY(0);
+    }
+}
+
+header{
+    animation: 1s ease-out 0s 1 slideInTop;
+}
+
 </style>
