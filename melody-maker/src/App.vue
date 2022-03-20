a6be7bee8feff8c1f29b7c86458ca315475744b7
Nate Harrell <nate.harrell@me.com>
2022-03-20 18:53:21 -0500

    working on carousel

--- melody-maker/src/App.vue
+++ melody-maker/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <Title />
+    <Carousel />
   </div>
 </template>
 
 <script>
 import Title from "@/components/Title.vue"
+import Carousel from "@/components/Carousel.vue"
+
 export default {
   name: "App",
   components:{
-    Title
+    Title,
+    Carousel
   }
 }
 </script>
