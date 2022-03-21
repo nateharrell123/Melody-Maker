1344116b1de5de63752eed514c964f2a14626221
Nate Harrell <nate.harrell@me.com>
2022-03-20 20:22:06 -0500

    select

--- melody-maker/src/components/Carousel.vue
+++ melody-maker/src/components/Carousel.vue
@@ -1,14 +1,31 @@
 <template>
-    <div>
-    </div>
+  <div class="container">
+      <b-card class="card">
+          <b-select class="">
+              <option value="1"> asdfasdf</option>
+              <option value="1"> 1 </option>
+              <option value="1"> 1 </option>
+              <option value="1"> 1 </option>
+
+          </b-select>
+      </b-card>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+
+};
 </script>
 
 <style scoped>
-
+.container{
+    padding-top:50px;
+}
+.card{
+    background:#222222;
+    height:500px;
+    width:1200px;
+    margin:auto;
+}
 </style>
