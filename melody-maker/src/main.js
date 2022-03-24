e89eb79424f1b9c505c3e3ed5f2537a14c492b19
Nate Harrell <nate.harrell@me.com>
2022-03-23 21:14:25 -0500

    :) package verison was wrong

--- melody-maker/src/main.js
+++ melody-maker/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { BootstrapVue } from 'bootstrap-vue'
 import Vuelidate from 'vuelidate'
-import {store} from './store'
+import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
