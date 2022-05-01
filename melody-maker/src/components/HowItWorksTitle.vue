1078df166e22a72221984f0f9a00e18cd25c7d05
Nate Harrell <nate.harrell@me.com>
2022-05-01 14:24:30 -0500

    commit

---
+++ melody-maker/src/components/HowItWorksTitle.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <div class="title">
+      <button @click="toHome()" id="title-text">melodies.io </button>
+    </div>
+  </header>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {
+  name: "HowItWorksTitle",
+  data(){
+    return{
+      hide: false
+    }
+  },
+  methods: {
+    ...mapMutations(["setCreatedMelody"]),
+    toHome(){
+      this.$router.push("/")
+    }
+  }
+};
+</script>
+
+<style scoped>
+.title{
+  margin-top: 30px;
+  font-family: "Montserrat";
+  text-align:left;
+  font-size: 75px;
+  padding-left:55px;
+  color:white;
+}
+#title-text{
+  background-image: linear-gradient(45deg, #eb1e82, #f88d30);
+    border:none;
+  font-weight: 550;
+
+  background-size: 100%;
+  letter-spacing: 5px;
+  background-repeat: repeat;
+
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+#title-text:hover{
+      background-image: linear-gradient(45deg, #ba0f62, #ff7700);
+
+}
+</style>
--- melody-maker/src/views/About.vue
+++ melody-maker/src/views/About.vue
@@ -1,12 +1,37 @@
 <template>
-  <div class="about" style="color:white">
-    <h1>This is an about page</h1>
+  <div class="title">
+    <HowItWorksTitle
+    style="padding-bottom:30px;" />
+      <b-row>
+        <b-col cols="1" />
+        <b-col cols="10">
+          <b-card class="content-card">
+            <h1 class="content-card-text-title">
+              How it works:
+            </h1>
+            <hr class="line" />
+            <p class="content-card-text">
+              Melodies.io assigns your melody to three "writers/composers":
+            </p>
+            <ul>
+              <li class="content-card-text"> List 1 </li>
+            </ul>
+          </b-card>
+        </b-col>
+        <b-col cols="1">
+        </b-col>
+      </b-row>
   </div>
+
 </template>
 
 <script>
+import HowItWorksTitle from "../components/HowItWorksTitle.vue"
 export default{
   name: "About",
+  components: {
+    HowItWorksTitle
+  },
   data(){
     return{
 
@@ -14,3 +39,26 @@ export default{
   }
 }
 </script>
+<style scoped>
+.content-card{
+  background: #32363b;
+  margin: auto;
+  box-shadow: 0 0 30px 5px #111;
+}
+.content-card-text-title{
+  font-family: "Montserrat";
+  font-size:50px;
+  color: #9AAAAA;
+  text-align: center;
+}
+.content-card-text{
+  font-family: "Montserrat";
+  font-size:22px;
+  color: #9AAAAA;
+}
+.line{
+  border-top: 3px solid white;
+  margin-left: 10%;
+  margin-right:10%;
+}
+</style>
