52424e4a6424c1a75134bdcdb8a6c7c19b279ab8
Nate Harrell <nate.harrell@me.com>
2022-05-01 15:17:59 -0500

    ill do it myself

--- melody-maker/src/views/About.vue
+++ melody-maker/src/views/About.vue
@@ -17,6 +17,31 @@
               <li class="content-card-text"> <span class="writer2-text"> Steve </span>
                 (ง^ᗜ^)ง
               </li>
+            <ul class="content-card-text">
+              <!-- <span class="accent-color-pink">Steve</span> plays a 
+              <span class="accent-color-red"> LOT </span>
+              of arcade video games. He enjoys the <span class="accent-color-pink"> fast paced, high energy </span>
+              stuff.
+              He favors eighth note rhythms: <br/>
+              <code> pattern.Note(key[phrase1[i]], MusicalTimeSpan.Eighth); </code>
+              <br />
+              and revolving around <span class="accent-color-pink"> chord tones: </span>
+              <br />
+              <code> int[] chordTones = new int[4] { 0, 2, 4, 7 }; </code>
+              <br />
+              <code>
+                pattern.Note(key[chordTones[StaticRandom.Instance.Next(0, 4)]], MusicalTimeSpan.Eighth);
+              </code> -->
+              <pre class="prettyprint">class Voila {
+<span class="accent-color-blue">public: </span>
+  // Voila
+  static const string VOILA = "Voila";
+
+  // will not interfere with embedded <a href="#voila2">tags</a>.
+}</pre>
+
+            </ul>
+            <hr class="line" />
               <li class="content-card-text"> <span class="writer3-text">Jerry </span>
                 (¬‿¬)
               </li>
@@ -31,7 +56,6 @@
         </b-col>
       </b-row>
   </div>
-
 </template>
 
 <script>
@@ -57,12 +81,12 @@ export default{
 .content-card-text-title{
   font-family: "Montserrat";
   font-size:50px;
-  color: #9AAAAA;
+  color: #1AAAAA;
   text-align: center;
 }
 .content-card-text{
   font-family: "Montserrat";
-  font-size:32px;
+  font-size:28px;
   color: #9AAAAA;
 }
 .writer1-text{
@@ -78,6 +102,17 @@ export default{
   font-size:38px;
 }
 .line{
-  border-top: 3px solid white;
+  border-top: 5px solid;
+  color: #9AAAAA;
+}
+.accent-color-red{
+  color:red;
+}
+.accent-color-blue{
+    color: #1AAAAA;
+}
+.accent-color-pink{
+    color: #eb1e82;
+
 }
 </style>
